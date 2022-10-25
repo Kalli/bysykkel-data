2b685c311f0ab920b5ca85c8346ad91f0fad05da
Karl Tryggvason <ktryggvason@gmail.com>
2022-10-25 21:59:22 +0200

    Skip updating csv file when bikes aren't available

--- postprocessing.js
+++ postprocessing.js
@@ -4,6 +4,12 @@ import { readJSON, writeTXT } from "https://deno.land/x/flat@0.0.15/mod.ts";
 const filename = Deno.args[0];
 const json = await readJSON(filename);
 
-json.data.stations.map((element) => {
-   writeTXT("station_status.csv", Object.values(element).join(",") + "\n", {append: true});
-});
+// we can't specify timezones for github workflow cron schedules, so check for Oslo time here
+const d = new Date();
+const hour = Number(d.toLocaleTimeString('en-GB', {timeZone: "Europe/Oslo"}).slice(0, 2));
+// Oslo bikes aren't accessible between 1 and 5, so we don't need to update within that time
+if (hour > 5 || hour < 1){
+   json.data.stations.map((element) => {
+      writeTXT("station_status.csv", Object.values(element).join(",") + "\n", {append: true});
+   });   
+} 
